69872655513170368b08a7f25aacf57f902abd45
jp-sanchezs <109883846+jp-sanchezs@users.noreply.github.com>
2023-04-26 18:07:42 -0600

    Implementacion de socket en el servidor

---
+++ AppWebCE/Servidor/Servidor.js
@@ -0,0 +1,31 @@
+
+// Se accede a las herramientas de net
+const net = require('net'); 
+
+// Se crea el servidor
+const servidor = net.createServer() 
+
+// Se inicia el Socket
+servidor.on('connection', (socket)=>{
+
+    // Recibe la informacion del cliente y la muestra
+    socket.on('data', (data)=>{
+        console.log('Mensaje recibido desde el cliente:' + data + '.')
+    })
+
+    // Avisa cuando cierra la comunicacion
+    socket.on('close', ()=>{
+        console.log('Se ha cerrado la comunicacion.')
+    })
+
+    // Muestra el error en caso de haber
+    socket.on('error', (error)=>{
+        console.log(error.message)
+    })
+})
+
+// Se escucha en determiando puerto
+servidor.listen(5000, ()=>{
+    console.log('Escuchando en el puerto:', servidor.address().port + '.')
+})
+
