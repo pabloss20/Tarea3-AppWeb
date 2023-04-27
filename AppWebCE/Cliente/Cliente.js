11ccb89c9f2ee77c2b512f327ef4e60553d5cce9
jp-sanchezs <109883846+jp-sanchezs@users.noreply.github.com>
2023-04-26 19:02:21 -0600

    Implementacion de la logica de envio de cliente a servidor

--- AppWebCE/Cliente/Cliente.js
+++ AppWebCE/Cliente/Cliente.js
@@ -2,6 +2,8 @@
 // Se accede a las herramientas de net
 const net = require('net');  
 
+const readline = require('readline-sync')
+
 // Puerto y host al cual se conecta el cliente
 const options = {
     port: 5000,
@@ -15,9 +17,23 @@ const cliente = net.createConnection(options)
 cliente.on('connect', ()=>{
     console.log('Conexion establecida con el servidor.')
     cliente.write('Hola, soy el cliente: estudianteCE.')
+    send()
 })
 
 // Muestra el error en caso de haber
 cliente.on('error', (error)=>{
     console.log(error.message)
 })
+
+// Funcion para enviar 
+function send(){
+
+    // Se almacena en una variable lo que se ingrese por el usuario
+    var line = readline.question('Digite algo: ')
+
+    if (line == "0"){
+        cliente.end()
+    }else{
+        cliente.write(line)
+    }
+}
