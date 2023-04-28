f1d448db69cce1dc52055e09fdd7b224ddc103f1
jp-sanchezs <109883846+jp-sanchezs@users.noreply.github.com>
2023-04-28 00:24:55 -0600

    Modificaciones en el frontend

---
+++ AppWebCE/Cliente/App.js
@@ -0,0 +1,15 @@
+import Wrapper from "./Componentes/Wrapper";
+
+function App()
+{
+    return
+    (
+        <div>
+            <Wrapper>
+                <h1>App</h1>
+            </Wrapper>
+        </div>
+    );
+}
+
+export default App;
