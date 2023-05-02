c7da6ae23fd8a8f264569a5114c2893a75ba6682
jp-sanchezs <109883846+jp-sanchezs@users.noreply.github.com>
2023-05-02 08:51:17 -0600

    Comunicación exitosa mediante Sockets

--- AppWebCE/server/index.js
+++ AppWebCE/server/index.js
@@ -1,23 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const express = require ('express')
-const app = express()
+app.use(cors());
 
-const http = require ('http')
-
-const {Server} = require ('socket.io')
-
-const cors = require ('cors')
-app.use(cors())
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5000",
-        methods: ["GET", "POST"],
-    },
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-server.listen(5001, ()=>{
-    console.log("SERVER IS RUNNING...")
-})
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
