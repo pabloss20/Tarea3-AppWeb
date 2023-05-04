c5a20ec83a6233c587589b4db9f25662f80985db
jp-sanchezs <109883846+jp-sanchezs@users.noreply.github.com>
2023-05-04 03:49:21 -0600

    Corrección de errores

--- AppWebCE/client/src/App.js
+++ AppWebCE/client/src/App.js
@@ -2,7 +2,21 @@ import "./App.css";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from './components/ButtonBox'
+import Button from './components/Button'
+import CalcProvider from "./context/CalcContext";
+
 const socket = io.connect("http://localhost:3001");
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 function App() {
   //Room State
@@ -27,25 +41,21 @@ function App() {
       setMessageReceived(data.message);
     });
   }, [socket]);
+
   return (
-    <div className="App">
-      <input
-        placeholder="Room Number..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}> Join Room</button>
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      {messageReceived}
-    </div>
+    <CalcProvider>
+      <Wrapper>
+        <Screen />
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Button
+              value={btn}
+              key={i}
+            />
+          ))}
+        </ButtonBox>
+      </Wrapper>
+    </CalcProvider>
   );
 }
 
