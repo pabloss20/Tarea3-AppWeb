f56dd0b433077fedd049e356340811935a2a2cb0
jp-sanchezs <109883846+jp-sanchezs@users.noreply.github.com>
2023-05-06 00:06:17 -0600

    Finalizacion de la documentacion interna

--- AppWebCE/client/src/App.js
+++ AppWebCE/client/src/App.js
@@ -1,208 +1,84 @@
-import { useReducer } from "react"
-import DigitButton from "./DigitButton"
-import OperationButton from "./OperationButton"
 
-import './style.css'
-import "./App.css";
+// requerimientos para socket.io y react js
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
+// creación/conexión del sockect al puerto del servidor
 const socket = io.connect("http://localhost:3001");
 
-export const ACTIONS = {
-  ADD_DIGIT: "add-digit",
-  CHOOSE_OPERATION: "choose-operation",
-  CLEAR: "clear",
-  DELETE_DIGIT: "delete-digit",
-  EVALUATE: "evaluate",
-}
-
-function reducer(state, { type, payload }) {
-  switch (type) {
-    case ACTIONS.ADD_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          currentOperand: payload.digit,
-          overwrite: false,
-        }
-      }
-      if (payload.digit === "0" && state.currentOperand === "0") {
-        return state
-      }
-      if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state
-      }
-
-      return {
-        ...state,
-        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      }
-    case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null) {
-        return state
-      }
-
-      if (state.currentOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-        }
-      }
-
-      if (state.previousOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-          previousOperand: state.currentOperand,
-          currentOperand: null,
-        }
-      }
-
-      return {
-        ...state,
-        previousOperand: evaluate(state),
-        operation: payload.operation,
-        currentOperand: null,
-      }
-    case ACTIONS.CLEAR:
-      return {}
-    case ACTIONS.DELETE_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          overwrite: false,
-          currentOperand: null,
-        }
-      }
-      if (state.currentOperand == null) return state
-      if (state.currentOperand.length === 1) {
-        return { ...state, currentOperand: null }
-      }
-
-      return {
-        ...state,
-        currentOperand: state.currentOperand.slice(0, -1),
-      }
-    case ACTIONS.EVALUATE:
-      if (
-        state.operation == null ||
-        state.currentOperand == null ||
-        state.previousOperand == null
-      ) {
-        return state
-      }
-
-      return {
-        ...state,
-        overwrite: true,
-        previousOperand: null,
-        operation: null,
-        currentOperand: evaluate(state),
-      }
-  }
-}
-
-function evaluate({ currentOperand, previousOperand, operation }) {
-
-  const prev = parseFloat(previousOperand)
-  const current = parseFloat(currentOperand)
-
-  const sendMessage = () => {
-    socket.emit("send_message", { message: "Hola"});
-  };
-
-  if (isNaN(prev) || isNaN(current)) return ""
-  let computation = ""
-  switch (operation) {
-    case "+":
-      computation = prev + current
-      break
-    case "-":
-      computation = prev - current
-      break
-    case "*":
-      computation = prev * current
-      break
-    case "÷":
-      computation = prev / current
-      break
-  }
-
-  return computation.toString()
-}
-
-const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
-  maximumFractionDigits: 0,
-})
-function formatOperand(operand) {
-  if (operand == null) return
-  const [integer, decimal] = operand.split(".")
-  if (decimal == null) return INTEGER_FORMATTER.format(integer)
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
-}
-
+// lógica principal
 function App() {
 
-    // Messages States
-    const [message, setMessage] = useState("");
-    const [messageReceived, setMessageReceived] = useState("");
-  
-    const sendMessage = () => {
-      socket.emit("send_message", { message: "Hola"});
-    };
-  
-    useEffect(() => {
-      socket.on("receive_message", (data) => {
-        setMessageReceived(data.message);
-      });
-    }, [socket]);
+  // envia los valores y la operación mediante un json al servidor, toma los valores de los input
+  const sendMessage = () => {
 
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
-    reducer,
-    {}
-  )
+    // toma los valores de los input
+    let val1 = document.querySelector('#num_one').value
+    let val2 = document.querySelector('#num_two').value
+    let op = document.querySelector('#operation').value
 
+    // se crea el json
+    let myjson = {
+      value1: val1,
+      value2: val2,
+      operation: op
+    }
+
+    // envia la data al servidor
+    socket.emit("send_message", myjson);
+  };
+
+  // espera respuesta del servidor
+  useEffect(() => {
+
+    // recibe y lee el json enviado por el servidor con el resultado de la operación
+    socket.on("receive_message", (server_json) => {
+
+      let resultado = server_json.resultado
+
+      // espera a ser actualizada la página NOTA:UNA VEZ EL SERVIDOR ENVIA RESPUESTA SE REFRESCA LA PÁGINA
+      // VER LA TERMINAL DEL SERVIDOR PARA OBSERVAR LOS VALORES, LA OPERACIÓN Y EL RESULTADO
+      const handle = event =>{
+        console.log(resultado)
+        document.querySelector('#result').value=resultado;
+        event.preventDefault()
+      };
+    });
+  }, [socket]);
+
+  useEffect(()=>{
+    document.querySelector('#result').value="";
+  },[])
+
+  // componentes de la app 
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">
-          {formatOperand(previousOperand)} {operation}
-        </div>
-        <div className="current-operand">{formatOperand(currentOperand)}</div>
+
+    <div className="App">
+
+      <div className="app-title">
+
+        <h1> Basic Form Calculator</h1>
+
       </div>
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
-        AC
-      </button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
-        DEL
-      </button>
-      <OperationButton operation="÷" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="*" dispatch={dispatch} />
-      <DigitButton digit="4" dispatch={dispatch} />
-      <DigitButton digit="5" dispatch={dispatch} />
-      <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} />
-      <DigitButton digit="7" dispatch={dispatch} />
-      <DigitButton digit="8" dispatch={dispatch} />
-      <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
-      <DigitButton digit="0" dispatch={dispatch} />
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-      >
-        =
-      </button>
+
+      <form>
+            <div>
+              <input type="text" id="result"  readOnly /> 
+            </div>
+            <div>
+              <input type="text" id="num_one" placeholder="First value..." />
+              <input type="text" id="num_two" placeholder="Second value..." />
+              <input type="text" id="operation" placeholder="Enter an operation (+)(-)(x)(/)" size="27"/>
+            </div>  
+            <div>
+              <button onClick={sendMessage}>Calculate</button>
+            </div>
+      </form>
+
     </div>
-  )
+
+  );
+
 }
 
-export default App
+export default App;
--- AppWebCE/server/index.js
+++ AppWebCE/server/index.js
@@ -1,3 +1,5 @@
+
+// requerimeintos propios de socket.io y react js
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -6,8 +8,10 @@ const cors = require("cors");
 
 app.use(cors());
 
+// Se crea el servidor
 const server = http.createServer(app);
 
+// conexión al puerto del servidor react js y métodos a utilizar
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -15,19 +19,67 @@ const io = new Server(server, {
   },
 });
 
+// inicia la conexión
 io.on("connection", (socket) => {
+
+  // notifica cuando un usario se conecta y muestra su id
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
+  // se recibe la data enviada por el cliente
+  socket.on("send_message", (client_json) => {
 
-  socket.on("send_message", (data) => {
-    console.log(toString(data));
-    socket.to(data.room).emit("receive_message", data);
+    // muestra los valores del json enviado por el cliente
+    console.log(typeof(parseFloat(client_json.value1)))
+    console.log(client_json.value2)
+    console.log(client_json.operation)
+
+    // variables que toman los valores envidados por el cliente mediante un json
+    let first_value = parseFloat(client_json.value1)
+    let second_value = parseFloat(client_json.value2)
+    let operation = client_json.operation
+    let result
+
+    // switch case donde se realizan las operaciones matemáticas suma, resta, división y multiplicación
+    switch (operation){
+      case "+":
+
+        result = first_value + second_value
+        break;
+
+      case "-":
+        result = first_value - second_value
+        break;
+
+      case "x":
+
+        result = first_value * second_value
+        break;
+
+      case "/":
+
+        result = first_value / second_value
+        break;
+
+      default:
+        break;
+    }
+
+    // muestra el resultado en la terminal
+    console.log(result)
+
+    // variable que castea a string el resultado de la oepracion
+    let resultado_string = toString(result) 
+
+    // se crea un json con el resultado de la operación para enviarla al cliente mediante socket
+    let server_json = {resultado: resultado_string}
+
+    // se envia la data al cliente
+    socket.send("receive_message", server_json)
+    
   });
 });
 
+// puerto al que escucha y se ejecuta el servidor
 server.listen(3001, () => {
   console.log("SERVER IS RUNNING");
 });
